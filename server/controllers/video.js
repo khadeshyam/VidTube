59461ad6896638793002494788add6d2000e5ad4
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-05-17 18:28:52 +0530

    init basic video routes and controllers

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,5 +1,58 @@
-import Video from '../models/Video.js';
+import User from "../models/User.js";
+import Video from "../models/Video.js";
+import { createError } from "../error.js";
 
-export const getVideo = async (req, res) => {
-      res.json("This is video route")
-}
+export const addVideo = async (req, res, next) => {
+  const newVideo = new Video({ userId: req.user.id, ...req.body });
+  try {
+    const savedVideo = await newVideo.save();
+    res.status(200).json(savedVideo);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found!"));
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      return next(createError(403, "You can update only your video!"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found!"));
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("The video has been deleted.");
+    } else {
+      return next(createError(403, "You can delete only your video!"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    res.status(200).json(video);
+  } catch (err) {
+    next(err);
+  }
+};
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -3,6 +3,10 @@ import { getVideo } from "../controllers/video.js";
 
 const router = express.Router();
 
-router.get("/",getVideo)
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, addVideo)
+router.delete("/:id", verifyToken, addVideo)
+router.get("/find/:id", getVideo)
+
 
 export default router;
