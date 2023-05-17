c98b697afca6308f0d1088e2fb29e68eca53d4a6
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-05-17 18:26:12 +0530

    init basic User routes and controllers

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,28 @@
-import express from "express";
-import {getUsers} from "../controllers/user.js";
+import express from 'express';
+import {updateUser,deleteUser,getUser,subscribeUser,unsubscribeUser,likeVideo,dislikeVideo} from '../controllers/user.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
 
-router.get("/",getUsers);
+//update an user
+router.put('/:id',verifyToken,updateUser)
+
+//delete an user
+router.delete('/:id',verifyToken,deleteUser);
+
+//get an user
+router.get('find/:id',verifyToken,getUser);
+
+//subscribe an user
+router.put('/sub/:id',verifyToken,subscribeUser);
+
+//unsubscribe an user
+router.put('/unsub/:id',verifyToken,unsubscribeUser);
+
+//like a video
+router.put('/like/:videoId',verifyToken,likeVideo);
+
+//unlike a video
+router.put('/dislike/:videoId',verifyToken,dislikeVideo);
 
 export default router;
