8a9bfbb7d1306200c51539f47647b6eeb846647b
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-05-17 11:44:20 +0530

    complete client

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
-import Menu from './components/Menu';
-import Navbar from './components/Navbar';
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import SignIn from "./pages/SignIn";
 
 const Container = styled.div`
-    display: flex;
+  display: flex;
 `;
-const Main = styled.div`
-    flex: 7;
-`
-const Wrapper = styled.div``
 
+const Main = styled.div`
+  flex: 6;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 20px 28px;
+`;
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <Container>
-      <Menu />
-      {/* main */}
-      <Main>
-        <Navbar />
-        <Wrapper>
-          video cards
-        </Wrapper>
-      </Main>
-    </Container>
-  )
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="signin" element={<SignIn />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
