df25020a9b32d83d35c20ba0d8b380727b797dab
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-05-18 17:24:43 +0530

    implement google auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -21,24 +21,40 @@ export const signup = async (req, res, next) => {
 };
 export const signin = async (req, res, next) => {
       try {
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) return next(createError(404, "User not found!"));
-    
-        const isCorrect = await bcrypt.compare(req.body.password, user.password);
-    
-        if (!isCorrect) return next(createError(400, "incorrect username or password!"));
-    
-        const token = jwt.sign({ id: user._id }, process.env.JWT);
-        const { password, ...others } = user._doc;
-    
-        res.cookie("access_token", token, {httpOnly: true,}).status(200).json(others);
-      } catch (err) {
-        next(err);
-      }
-    };
-    
-export const googleAuth = async (req, res, next) => {
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) return next(createError(404, "User not found!"));
 
+            const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+            if (!isCorrect) return next(createError(400, "incorrect username or password!"));
+
+            const token = jwt.sign({ id: user._id }, process.env.JWT);
+            const { password, ...others } = user._doc;
+
+            res.cookie("access_token", token, { httpOnly: true, }).status(200).json(others);
+      } catch (err) {
+            next(err);
+      }
 };
 
+export const googleAuth = async (req, res, next) => {
+      try {
+            console.log(req.body);
+            const user = await User.findOne({ email: req.body.email });
+            if (user) {
+                  const token = jwt.sign({ id: user._id }, process.env.JWT);
+                  res.cookie("access_token", token, {httpOnly: true}).status(200).json(user._doc);
+            } else {
+                  const newUser = new User({
+                        ...req.body,
+                        fromGoogle: true,
+                  });
+                  const savedUser = await newUser.save();
+                  const token = jwt.sign({ id: savedUser._id }, process.env.JWT);
+                  res.cookie("access_token", token, {httpOnly: true,}).status(200).json(savedUser._doc);
+            }
+      } catch (err) {
+            next(err);
+      }
+};
 
