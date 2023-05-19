295af655de5d3a9088a61c3bfac7cee87cb781e9
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-05-19 19:11:00 +0530

    hide firebase credentials

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvZCxh8ndETaIUukIvmPpO_Vdu7eiJvlQ",
-  authDomain: "video-7c7a3.firebaseapp.com",
-  projectId: "video-7c7a3",
-  storageBucket: "video-7c7a3.appspot.com",
-  messagingSenderId: "8315631699",
-  appId: "1:8315631699:web:f91f60773c520864042ea6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
